54cb99be00b8d45ad8f16e6bdc658847876af0a9
KhuongD <71280822+KhuongD@users.noreply.github.com>
2020-09-25 00:33:04 -0700

    I'm done with the assignment
    I face some trouble trying to connect the github desktop and Clion so I couldn't save step by step but I'm able to submit the final one.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,3 @@
-/**
- * Author:	Carlos R. Arias
- * Class:	CSC 2330 Data Structures and Programming
- * Date:	September 23 / 2020
- * Purpose:	A simple number guessing game
- */
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -14,46 +8,42 @@ using std::endl;
 using std::cerr;
 
 int main() {
-	srand(time(0));
 
-	int numberToGuess;
-	int userGuess;
-	int maximumGuesses, maximumToGuess;
-	bool won = false;
-	int keepGoing = 0;
+    int maxChance;
+    int maxInput;
+    int playerGuess;
+    int moreGame = 1;
+    do {
+        cout << " Max range that you want :" << endl;
+        cin >> maxInput;
+        cout << " Max chance that you want :" << endl;
+        cin >> maxChance;
+        srand(time(0));
+        int guessNumber = rand() % maxInput + 1;
+        cout << "--" << guessNumber << endl;
+        for (int a=1; a < maxChance +1; a++){
+            cout << " Player's input number "<< a <<" :" << endl;
+            cin >> playerGuess;
+            if (playerGuess == guessNumber){
+                cout << "Congrats you have won !!"<< endl;
 
-	do{
-		cout << "What is the maximum number to guess: ";
-		cin >> maximumToGuess;
-		cout << "How many opportunities you want: ";
-		cin >> maximumGuesses;
+                break;
+            }
+            else {
+                if (a== maxChance){
+                    cout <<" You ran out of chances my friend !!"<< endl;
+                }
+                else{
+                    cout << "Try again my dude !!" << endl;
+                }
 
-		numberToGuess = rand() % maximumToGuess + 1;
-		cout << "- - - - " << numberToGuess << " - - - -" << endl << endl;
-		for (int i = 0; i < maximumGuesses; ++i) {
-			cout << "Give me your guess #" << i + 1 << ": ";
-			cin >> userGuess;
-			if (userGuess == numberToGuess){
-				won = true;
-				break;
-			}else{
-				cout << "Guess Again..." << endl;
-			}
-		}
-		/**
-		 * if (won == true) {} --> How you should write
-		 * if (won) {} --> you need to be able to read it
-		 */
-		if (won){
-			cout << "You are a Winner!!!" << endl;
-		}else{
-			cout << "Loser :(" << endl;
-		}
-		cout << "Would you like to play another game? [1 for yes]: ";
-		cin >> keepGoing;
-	}while (keepGoing == 1);
+            }
+        }
+        cout<< "Would you like to play one more game ? (Type 1 for another one)" << endl;
+        cin >> moreGame;
+    }   while (moreGame==1);
 
-	cout << "Thanks for playing my game" << endl;
+    cout <<" Thank you for playing !!"<< endl;
 
-	return 0;
+    return 0;
 }
